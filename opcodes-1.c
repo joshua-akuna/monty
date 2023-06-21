ba1c2a5ec4b285c452916563dc448c7bbbb4eead
Joshua Akuna <akunajoshua@gmail.com>
2023-06-21 18:35:37 +0100

    opcodes-1.c : correction-1
    removed debug text

--- opcodes-1.c
+++ opcodes-1.c
@@ -78,5 +78,5 @@ void pint(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("PINT====>%d\n", (*stack)->n);
+	printf("%d\n", (*stack)->n);
 }
